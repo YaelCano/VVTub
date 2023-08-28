c343e4c8ce6fcd710b992fb9fb2b7f1b5f7144e5
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-27 20:53:45 -0500

    feat :sparkles: Cambio de funcion

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+
+/**
+ *  !JS boton superior del Menu principal
+ */
 var menuIcon = document.querySelector(".menu-icon"); // Agregamos un punto para seleccionar por clase
 var sidebar = document.querySelector(".siderbar");    // Agregamos un punto para seleccionar por clase
 var container = document.querySelector(".container"); // Agregamos un punto para seleccionar por clase
 
-menuIcon.addEventListener("click", function() { // Usamos addEventListener en lugar de onClick
+menuIcon.onclick = function() { 
     sidebar.classList.toggle("siderbar");
     container.classList.toggle("large-container");
-});
+}
