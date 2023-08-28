f8421dab37a311faf1f93caa639671935014ac32
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-28 13:30:57 -0500

    feat :sparkles: cambios al index y agrega funciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const path = "videos"
-let ReprodiccionVideo = async =>{
-    let peticio =
-}
+import { SearchAll } from "./get.js";
+document.querySelector("#chartSearch").addEventListener("change",(e)=>{
+    SearchAll(e.target.value)
+});
