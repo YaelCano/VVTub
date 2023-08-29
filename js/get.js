6a2ab8d02f0b8a8852490661594c1af145a57b23
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-29 01:09:11 -0500

    feat :sparkles: elimacion texto y comentacion

--- js/get.js
+++ js/get.js
@@ -1,26 +1,26 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '219c9d8956msh59d5178ab85fbf9p1159e9jsn66a291f5d2e4',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '219c9d8956msh59d5178ab85fbf9p1159e9jsn66a291f5d2e4',
+// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+// 	}
+// };
 
 
-export  const SearchAll = async(p1)=>{
-    options.method = 'GET';
-    const peticion = await fetch(`https://youtube138.p.rapidapi.com/channel/search/?id=UC8fkwsjcI_MhralEX1g4OBw&q=${p1}&hl=en&gl=US`, options);
-    const json = await peticion.json();
-    let h = 0, cont = 0;
-    let array = json.contents.map((val,id)=>{
-        if(val.playlist) return undefined;
-        cont++;
-        if(cont<=10) h = 30*cont;
+// export  const SearchAll = async(p1)=>{
+//     options.method = 'GET';
+//     const peticion = await fetch(`https://youtube138.p.rapidapi.com/channel/search/?id=UC8fkwsjcI_MhralEX1g4OBw&q=${p1}&hl=en&gl=US`, options);
+//     const json = await peticion.json();
+//     let h = 0, cont = 0;
+//     let array = json.contents.map((val,id)=>{
+//         if(val.playlist) return undefined;
+//         cont++;
+//         if(cont<=10) h = 30*cont;
 
-        return `<li><a href=""></a></li>`;
-    })
-    document.querySelector("#active").style.heigth =`${h}px`;
-    document.querySelector("#SearchAll").innerHTML = null;
-    Document.querySelector("#SearchAll").insertAdjacentHTML("beforeend", array.join(" "));
+//         return `<li><a href=""></a></li>`;
+//     })
+//     document.querySelector("#active").style.heigth =`${h}px`;
+//     document.querySelector("#SearchAll").innerHTML = null;
+//     Document.querySelector("#SearchAll").insertAdjacentHTML("beforeend", array.join(" "));
     
-}
+// }
--- js/main.js
+++
@@ -1,4 +0,0 @@
-import { SearchAll } from "./get.js";
-document.querySelector("#chartSearch").addEventListener("change",(e)=>{
-    SearchAll(e.target.value)
-});
